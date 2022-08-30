8155de544f99affb7b8d6e3573df5a5c6d307d4d
Roeman1505 <robinsonemmanuel1505@gmail.com>
2022-08-30 14:03:44 +0300

    Configuration of static files

--- begin/settings.py
+++ begin/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIR =[
+    BASE_DIR / 'static',
+    # os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
